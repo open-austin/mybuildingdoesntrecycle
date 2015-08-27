1f5fa9000445cb3ac9c53dd924631285933a6fa4
Luqmaan <ldawoodjee@gmail.com>
2015-08-26 19:14:38 -0500

    Change coords to Austin

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,7 @@
   WIMR.createMap = function(id) {
     var map = L.map(id, {
       maxZoom: 20
-    }).setView([41.881, -87.629], 11);
+  }).setView([30.267153, -97.743061], 14);
     var self = this;
     self.locations = {};
     self.pending = [];
@@ -31,7 +31,7 @@
       shadowAnchor: [13, 41],
       popupAnchor: [0, -36]
     });
-    
+
     map.wimrRefreshLocations = function(callback) {
       $.get('/locations.json')
       .done(function(response){
@@ -43,19 +43,19 @@
             locs[current_loc._id] = true;
           }
         }
-  
+
         map.clearPendingLocations();
-  
+
         $(window).trigger('locationsLoaded')
         if (callback) {
           callback();
         }
-        
+
       });
     }
-  
+
     map.dropPin = function(lat, lng, opts) {
-  
+
       var mkr = L.marker([lat,lng]);
       mkr.setIcon(activeIcon);
       mkr.addTo(map);
@@ -63,39 +63,39 @@
         mkr.bindPopup(opts.popupText);
         mkr.openPopup();
       }
-      
+
       map.setView([lat,lng], 16);
       return mkr;
     }
-    
+
     map.zoomToPin = function(loc) {
       var mkr = self.locations[loc._id];
       var lat = mkr.latitude;
       var lng = mkr.longitude;
-      
+
       map.setView([lat,lng], 16);
-      
+
       for (var id in self.locations) {
         self.locations[id].setIcon(defaultIcon);
       }
       mkr.setIcon(activeIcon);
       mkr.openPopup();
     }
-    
+
     map.addOrUpdateLocation = function(loc) {
       var txt = loc.address + "<br>";
       txt += loc.reports.length;
       txt += loc.reports.length === 1 ? " report" : " reports";
-  
+
       if (!self.locations[loc._id]) {
         var lat = loc.latitude;
         var lng = loc.longitude
-    
+
         var marker = L.marker([lat,lng]);
         self.clusterGroup.addLayer(marker);
         marker.bindPopup(txt);
         marker.update();
-        
+
         marker._id = loc._id;
         marker.latitude = lat;
         marker.longitude = lng;
@@ -109,53 +109,53 @@
         marker.bindPopup(txt);
         marker.update();
       }
-      
+
       marker.on('click', function(e){
         window.location.hash = '/reports/' + this._id;
       })
     }
-    
+
     map.wimrReset = function(done) {
-      
-      map.setView([41.881, -87.629], 11);
+
+      map.setView([30.267153, -97.743061], 14);
       map.wimrRefreshLocations(function(){
         for (var loc in self.locations) {
           var mkr = self.locations[loc];
-          
+
           // not sure why it's necessary to open then close,
           // but the popup won't close with just .closePopup()
           mkr.openPopup().closePopup();
           mkr.setIcon(defaultIcon);
         }
-        
+
         if (done) {
           done(self);
         }
       });
     };
-    
+
     map.wimrHighlightPin = function(loc_id) {
       var mkr = self.locations[loc_id];
       mkr.openPopup();
       mkr.setIcon(activeIcon)
     };
-    
+
     map.wimrClosePopup = function(loc_id) {
       var mkr = self.locations[loc_id];
       mkr.closePopup();
     };
-    
+
     map.addPendingLocation = function(loc) {
       self.pending.push(loc);
     };
-    
+
     map.clearPendingLocations = function() {
       self.pending.forEach(function(mkr){
         self.clusterGroup.removeLayer(mkr);
       });
       self.pending = [] ;
     };
-    
+
     map.getLocation = function(id) {
       return self.locations[id]
     };
@@ -164,7 +164,7 @@
       maxZoom: 18,
       attribution: 'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>'
     }).addTo(map);
-    
+
     map.wimrRefreshLocations();
     return map;
   }
